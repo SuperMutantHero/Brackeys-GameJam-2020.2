e98a142deae07619c286217a926f9457771822e1
Devanshu Mishra <mishradev1910@gmail.com>
2020-07-30 16:14:05 +0530

    I have added a folder with playable scene..... Its has player script attached

---
+++ DevGa.me/Script/ItemPickup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+	void OnTriggerEnter2D(Collider2D Col)
+	{
+		if (Col.CompareTag("Collectible"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ DevGa.me/Script/PlatformerMovement.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlatformerMovement : MonoBehaviour
+{
+	//Speed control items
+	public float speed = 10;
+	public float jumpConstant = 10;
+
+	//Jump data items
+	public Transform foot;
+	public float radius;
+	public LayerMask groundMask;
+
+	//References
+
+
+	private float inputX;
+	private Rigidbody2D rb;
+
+	private bool isGrounded;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		inputX = Input.GetAxisRaw("Horizontal");
+
+		GroundCheck();
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (isGrounded)
+			{
+				Jump();
+			}
+		}
+	}
+
+	void FixedUpdate()
+	{
+		rb.velocity = new Vector2(inputX * speed, rb.velocity.y);
+	}
+
+	void Jump()
+	{
+		rb.velocity = new Vector2(rb.velocity.x, jumpConstant);
+	}
+
+	void GroundCheck()
+	{
+		if (Physics2D.OverlapCircle(foot.position, radius, groundMask))
+		{
+			isGrounded = true;
+		}
+		else {
+			isGrounded = false;
+		}
+	}
+
+	//Delete it after visualisation, it can cause performance issues
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawSphere(foot.position, radius);
+	}
+}
