da244a8d0262d2b81f7a6b4bf23c80fe215ebed3
Devanshu Mishra <mishradev1910@gmail.com>
2020-07-31 02:48:28 +0530

    New project

---
+++ DevGa.me/Script/RewindManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+//b48f654f-d0da-4848-9164-f714b574b271
+//b48f654f-d0da-4848-9164-f714b574b271
+
+public class RewindManager : MonoBehaviour
+{
+	public List<float> x_positions;
+	public List<float> y_positions;
+
+	public float timeToStore;
+
+	void Start()
+	{
+		InvokeRepeating("StorePositions", 0, timeToStore);
+	}
+
+	public void StorePositions()
+	{
+		if (x_positions.Count >= 1)
+		{
+			if (Mathf.Abs(x_positions[x_positions.Count - 1] - transform.position.x) > 0.01f)
+			{
+				float position = (float)Math.Round(transform.position.x, 3);
+				x_positions.Add(position);
+			}
+		}
+		else {
+
+			float position = (float)Math.Round(transform.position.x, 3);
+			x_positions.Add(position);
+		}
+
+	}
+}
